fb096dc4be222d3b15d7511a4581d00adf2a6f40
Christian <ramoschristian39@gmail.com>
2023-03-17 12:49:27 -0400

    Created secondary WinForm for Invidual Plan

---
+++ Coti4150Asig02/IndividualPlanForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coti4150Asig02
+{
+    public partial class IndividualPlanForm : Form
+    {
+        public IndividualPlanForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
